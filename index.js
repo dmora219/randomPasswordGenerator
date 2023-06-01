75eb5d33ed2e69e9c79f41400721ff3928af020d
dmora219 <dmora14633@gmail.com>
2023-06-01 03:54:35 -0500

    final

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",]
 //get random characters
 //generate random code
-let boxEl = document.getElementsByClassName("innerText");
-let boxElTwo = document.getElementsByClassName("innerTextTwo");
+let boxEl = document.querySelector(".innerText");
+let boxElTwo = document.querySelector(".innerText2");
 let btn = document.getElementById("generate-btn");
 let passwordLength = 12;
 
 function getRandomChar() {
-   let random = Math.floor(Math.random) * characters.length;
+   let random = Math.floor(Math.random() * characters.length);
    return characters[random]
 }
-btn.addEventListener("click",function(){
+function generatePassword() {
    let password = "";
-   for (let i = 0; i < passwordLength; i++){
-      password += getRandomChar()
-      console.log(password)
+   let passwordTwo = ""
+   for (let i = 0; i < passwordLength; i++) {
+      password += getRandomChar();
+      passwordTwo += getRandomChar();
    }
-   return password;
-})
-
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-let passwordLength = 12
-
-function getRandomCharacter() {
-    let randomChar = Math.floor(Math.random() * characters.length)
-    return characters[randomChar]
+   boxEl.textContent = password;
+   boxElTwo.textContent = passwordTwo;
 }
 
-function generateRandomPassword() {
-    let randomPassword = ""
-    for (let i = 0; i < passwordLength; i++) {
-        randomPassword += getRandomCharacter()         
-    }
-    return randomPassword
-}
-
-const generatedPasswordOne = generateRandomPassword()
-
-console.log("Here is a random password: ", generatedPasswordOne)
+btn.addEventListener("click", generatePassword);
